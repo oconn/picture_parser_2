16468b1fd038738ceab0313bdd3ea98c98d36940
Neal Peters <nealcpeters@gmail.com>
2014-03-08 21:59:08 -0600

    image upload functional

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -6,8 +6,9 @@ class PicturesController < ApplicationController
 
 	def create
     @picture = Picture.new(picture_params)
-    
+    puts picture_params
     if @picture.save
+    	parse_picture(@picture.picture_image.url(:original, timestamp: false), @picture)
       redirect_to "/"
     else
       render "new"
@@ -37,7 +38,24 @@ class PicturesController < ApplicationController
 
 	private
 
+	def parse_picture(url, object)
+		puts url
+		puts object
+	  # unless url[-4..-1] != ".JPG" && url[-4..-1] != ".jpg"
+	  image_in_question = EXIFR::JPEG.new("#{Rails.root}/public/#{url}")
+
+	  puts image_in_question
+
+	  object.update_attributes(
+	    latitude: image_in_question.gps.latitude, 
+	    longitude: image_in_question.gps.longitude, 
+	    date_time_original: image_in_question.date_time_original,
+	    date_time: image_in_question.date_time,
+	    gps_date_stamp: image_in_question.gps_date_stamp)
+
+	end
+
 	def picture_params
-    params.require(:picture).permit(:picture_images)
+    params.require(:picture).permit(:picture_image)
 	end
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,8 +1,8 @@
 class Picture < ActiveRecord::Base
 	
-  validates :longitude, presence: true
-  validates :latitude, presence: true
+  # validates :longitude, presence: true
+  # validates :latitude, presence: true
 
-  has_attached_file :picture_image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :picture_image, :styles => { :original => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :picture_image, :content_type => /\Aimage\/.*\Z/
 end
