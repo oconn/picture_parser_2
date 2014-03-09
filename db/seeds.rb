42861007f0b040f6039e5a07aa960cb2cd3bddff
Eric Stensland <estensland88@gmail.com>
2014-03-08 21:51:18 -0600

    seed will import non-gps data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,21 @@ require 'exifr'
 Dir.foreach('app/assets/images') do |photo|
 
   unless photo[-4..-1] != ".JPG" && photo[-4..-1] != ".jpg"
-  image_in_question = EXIFR::JPEG.new("app/assets/images/#{photo}")
+    image_in_question = EXIFR::JPEG.new("app/assets/images/#{photo}")
 
-  Picture.create(
-    latitude: image_in_question.gps.latitude, 
-    longitude: image_in_question.gps.longitude, 
-    date_time_original: image_in_question.date_time_original,
-    date_time: image_in_question.date_time,
-    gps_date_stamp: image_in_question.gps_date_stamp)
+    if image_in_question.gps == nil
+      Picture.create(
+        path: "app/assets/images/#{photo}",
+        date_time_original: image_in_question.date_time_original,
+        date_time: image_in_question.date_time)
+    else
+      Picture.create(
+        path: "app/assets/images/#{photo}",
+        latitude: image_in_question.gps.latitude, 
+        longitude: image_in_question.gps.longitude, 
+        date_time_original: image_in_question.date_time_original,
+        date_time: image_in_question.date_time,
+        gps_date_stamp: image_in_question.gps_date_stamp)
+    end
   end
 end
